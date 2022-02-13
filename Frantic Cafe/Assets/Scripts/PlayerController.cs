eecc15f52d2e466e783317b6997a0cbf2d82b164
gil <gilbertzhang1208@gmail.com>
2022-02-12 19:32:35 -0500

    player walk animation

--- Frantic Cafe/Assets/Scripts/PlayerController.cs
+++ Frantic Cafe/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,31 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     Rigidbody2D rb;
+    Animator animator;
+    Renderer spriteRenderer;
+
     Vector2 velocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
+
+        animator.enabled = velocity.magnitude != 0;
     }
 
     void FixedUpdate()
     {
+        if (velocity.x != 0)
+            spriteRenderer.transform.localScale = new Vector3(Mathf.RoundToInt(velocity.x), 1, 1);
+
         rb.MovePosition(rb.position + velocity.normalized * Time.deltaTime * moveSpeed);
     }
 }
